2f984c7eaff0912d9690289b4eb86d2227e80301
RafaelGSanches03 <rafaelgsanches03@gmail.com>
2026-06-17 21:33:22 -0300

    Front melhorado

--- frontend/script.js
+++ frontend/script.js
@@ -71,17 +71,83 @@ async function listarProdutos(){
     produtos.forEach(produto => {
 
         html += `
-        <p>
-            ${produto.nome}
-            -
-            R$ ${produto.preco}
-            -
-            Quantidade: ${produto.quantidade}
-        </p>
+        <div class="produto">
+
+        <h3>${produto.nome}</h3>
+
+        <p>💰 Preço: R$ ${produto.preco}</p>
+
+        <p>📦 Quantidade: ${produto.quantidade}</p>
+
+        <div class="acoes">
+
+                <button class="editar"
+                    onclick="editarProduto('${produto._id}')">
+                    ✏️ Editar
+                </button>
+
+                <button class="excluir"
+                    onclick="excluirProduto('${produto._id}')">
+                    🗑️ Excluir
+                </button>
+
+            </div>
+
+            </div>
         `;
 
     });
 
     document.getElementById("produtos").innerHTML = html;
+
+}
+
+async function excluirProduto(id){
+
+    if(!confirm("Deseja realmente excluir este produto?")){
+        return;
+    }
+
+    await fetch(
+        `http://localhost:3000/produtos/${id}`,
+        {
+            method:"DELETE",
+            headers:{
+                "Authorization": token
+            }
+        }
+    );
+
+    listarProdutos();
+
+}
+
+async function editarProduto(id){
+
+    const novoNome = prompt("Novo nome:");
+    const novoPreco = prompt("Novo preço:");
+    const novaQuantidade = prompt("Nova quantidade:");
+
+    if(!novoNome || !novoPreco || !novaQuantidade){
+        return;
+    }
+
+    await fetch(
+        `http://localhost:3000/produtos/${id}`,
+        {
+            method:"PUT",
+            headers:{
+                "Content-Type":"application/json",
+                "Authorization": token
+            },
+            body: JSON.stringify({
+                nome: novoNome,
+                preco: novoPreco,
+                quantidade: novaQuantidade
+            })
+        }
+    );
+
+    listarProdutos();
 
 }
